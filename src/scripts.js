63f28393bbc85cfa2ab538b31c5cb11561e9798f
Fabricio Santana <fabricio@Fabricios-MacBook-Pro.local>
2024-11-30 19:39:09 -0500

    added dotenv

--- src/scripts.js
+++ src/scripts.js
@@ -3,3 +3,12 @@ import { Octokit } from "octokit";
 const octokit = new Octokit({ 
     auth: 'SECRET_API_KEY'
   });
+
+const request = await octokit.request('GET /users/fabsantana', {
+    username: 'fabsantana',
+    headers: {
+        'X-GitHub-Api-Version': '2022-11-28'
+    }
+})
+
+console.log(request)
